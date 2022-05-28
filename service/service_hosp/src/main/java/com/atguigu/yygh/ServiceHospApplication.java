4d944fd645fc4dd0b3fb1a61a3704dee426120b0
xsLeo <2532413887@qq.com>
2022-05-28 09:59:32 +0800

    尚医通代码第一次提交

---
+++ service/service_hosp/src/main/java/com/atguigu/yygh/ServiceHospApplication.java
@@ -0,0 +1,14 @@
+package com.atguigu.yygh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ */
+@SpringBootApplication
+public class ServiceHospApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceHospApplication.class, args);
+    }
+}
